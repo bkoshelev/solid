182967c4b2abde24d5f7d409a20cda1690877ed0
bkoshelev <brskoshelev@gmail.com>
2022-05-05 19:26:56 +0300

    add pwa support

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withSourceMaps = require('@zeit/next-source-maps')
 const withCSS = require('@zeit/next-css')
+const withPWA = require('next-pwa')
 
 const withMDX = require('@next/mdx')({
   extension: /\.(md|mdx)$/,
@@ -11,7 +12,10 @@ const withMDX = require('@next/mdx')({
 module.exports = withCSS(
   withMDX(
     withSourceMaps(
-      { pageExtensions: ["md", "mdx", "tsx"] }
+      { pageExtensions: ["md", "mdx", "tsx"] },
+      pwa: {
+        dest: 'public'
+      }
     )
   )
 )
