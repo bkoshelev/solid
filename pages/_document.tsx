4f17cfecde99fab6b7f2138552f846919bfba088
bkoshelev <brskoshelev@gmail.com>
2022-05-05 21:10:41 +0300

    add link to manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Head, Main, DocumentContext, NextScript } from 'next/document'
+import Document, { DocumentContext, Head, Main, NextScript } from 'next/document'
 import React from 'react'
 import { ServerStyleSheet } from 'styled-components'
 
@@ -29,9 +29,9 @@ export default class MyDocument extends Document<IProps> {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
           <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#f8364c" />
+          <link rel='manifest' href='/favicon/site.webmanifest' />
           <meta name="msapplication-TileColor" content="#f8364c" />
           <meta name="theme-color" content="#ffffff" />
-
           <meta property="og:type" content="article" />
           <meta property="og:title" content="SOLID BOOK" />
           <meta property="og:site_name" content="Open Tech Authors, SOLID" />
